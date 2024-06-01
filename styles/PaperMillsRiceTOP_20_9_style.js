da15c99456d5211af6b7899211e06bafb042dd2c
Martin Nuñez Igarzabal <martin.mr1367@gmail.com>
2024-06-01 00:21:46 -0300

    Initial commit

---
+++ styles/PaperMillsRiceTOP_20_9_style.js
@@ -0,0 +1,50 @@
+var size = 0;
+var placement = 'point';
+
+var style_PaperMillsRiceTOP_20_9 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("VALUE Rice");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 162092.000000 && value <= 181742.333333) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.0 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(43,175,40,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 181742.333333 && value <= 202953.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.0 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,255,98,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 202953.000000 && value <= 270494.000000) {
+            style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 6.0 + size, points: 3,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
